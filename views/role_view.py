ce78f0b3403b16c7e6dd4c0f565cdd21bf83be57
CuTrung <cuphanthanhtrung4112003@gmail.com>
2025-09-27 22:20:26 +0700

    chore: update status success

--- views/role_view.py
+++ views/role_view.py
@@ -2,8 +2,8 @@ from fastapi.responses import JSONResponse
 from fastapi import status
 from fastapi.encoders import jsonable_encoder
 
-def success_response(data, message="Thành công"):
-    return JSONResponse(content={"message": message, "data": jsonable_encoder(data)}, status_code=200)
+def success_response(data, message="Thành công", status_code=status.HTTP_200_OK):
+    return JSONResponse(content={"message": message, "data": jsonable_encoder(data)}, status_code=status_code)
 
 
 def error_response(message="Lỗi", status_code=status.HTTP_400_BAD_REQUEST):
